9103d1298c59826f7956c7d2188a14863812a6bf
mohamed ibrahim <mohamedibrahim1223@gmail.com>
2021-11-14 15:39:02 +0300

    Move URLSessionHTTPClient to it's own file.

---
+++ GHFollowers/Networking/URLSessionHTTPClient.swift
@@ -0,0 +1,32 @@
+//
+//  URLSessionHTTPClient.swift
+//  GHFollowers
+//
+//  Created by MIF50 on 14/11/2021.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient {
+    
+    private let session: URLSession
+    
+    public init(_ session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnExpectedValuesRepresentation: Error { }
+    
+    public func get(from url: URL,completion:@escaping ((HTTPClientResult)-> Void)) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnExpectedValuesRepresentation()))
+            }
+            
+        }.resume()
+    }
+}
